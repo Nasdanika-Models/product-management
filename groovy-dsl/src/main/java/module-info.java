a5f028be0e9fc8a4f5800643a3de9ebb3573144e
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2026-06-24 03:09:26 -0400

    Ignoring target

---
+++ groovy-dsl/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.nasdanika.models.productmanagement.dsl.groovy {
+
+	exports org.nasdanika.models.productmanagement.dsl.groovy;
+
+	// transitive: the exported builders are Groovy classes whose public API exposes
+	// groovy.lang.Closure / GroovyObject, so consumers (e.g. the handlers module) need Groovy too.
+	requires transitive org.apache.groovy;
+	requires transitive org.nasdanika.models.productmanagement;
+
+}
