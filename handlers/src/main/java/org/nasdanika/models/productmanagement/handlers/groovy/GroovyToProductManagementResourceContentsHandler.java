6c649184aa1795ec6189d4aaaed479bbbf53afda
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2026-06-26 09:49:44 -0400

    Refactoring to Global DSL

--- handlers/src/main/java/org/nasdanika/models/productmanagement/handlers/groovy/GroovyToProductManagementResourceContentsHandler.java
+++ handlers/src/main/java/org/nasdanika/models/productmanagement/handlers/groovy/GroovyToProductManagementResourceContentsHandler.java
@@ -1,20 +1,10 @@
 package org.nasdanika.models.productmanagement.handlers.groovy;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import javax.script.Bindings;
 import javax.script.CompiledScript;
-import javax.script.ScriptException;
 
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.nasdanika.capability.emf.ResourceContentsHandler;
-import org.nasdanika.models.productmanagement.ProductmanagementPackage;
-import org.nasdanika.models.productmanagement.dsl.groovy.DslContext;
+import org.nasdanika.groovy.DslResourceContentsHandler;
 
 /**
  * Transform handler for the {@code .pm} qualifier over a {@code .groovy} source. It resolves the
@@ -23,66 +13,11 @@ import org.nasdanika.models.productmanagement.dsl.groovy.DslContext;
  * id-based references, then normalizes the result to {@code EObject[]}. Mirrors
  * {@code MarkdownToProductManagementResourceContentsHandler}.
  */
-public class GroovyToProductManagementResourceContentsHandler implements ResourceContentsHandler<EObject[]> {
-
-	private ResourceContentsHandler<CompiledScript> sourceHandler;
-
-	private Resource resource;
+public class GroovyToProductManagementResourceContentsHandler extends DslResourceContentsHandler {
 
 	public GroovyToProductManagementResourceContentsHandler(Resource resource, ResourceContentsHandler<CompiledScript> sourceHandler) {
-		this.resource = resource;
-		this.sourceHandler = sourceHandler;
-	}
-
-	@Override
-	public EObject[] load(InputStream inputStream, Map<?, ?> options) throws IOException {
-		try {
-			CompiledScript compiledScript = sourceHandler.load(inputStream, options);
-	
-			DslContext dsl = new DslContext(ProductmanagementPackage.eINSTANCE, resource);
-			Bindings bindings = compiledScript.getEngine().createBindings();
-			dsl.installInto(bindings);
-	
-			Object result = compiledScript.eval(bindings);
-			dsl.resolveDeferred();
-	
-			return normalize(result, dsl);
-		} catch (ScriptException e) {
-			throw new IOException(e);
-		}
-	}
-
-	/**
-	 * Normalizes the script result into resource contents. A returned {@code EObject}, array or
-	 * iterable of {@code EObject}s is flattened; if the script returned nothing usable, the elements
-	 * built through the DSL entry points (e.g. {@code product { }}) are used instead.
-	 */
-	private EObject[] normalize(Object result, DslContext dsl) {
-		List<EObject> contents = new ArrayList<>();
-		collect(result, contents);
-		if (contents.isEmpty()) {
-			contents.addAll(dsl.getRoots());
-		}
-		return contents.toArray(EObject[]::new);
-	}
-
-	private void collect(Object value, List<EObject> contents) {
-		if (value instanceof EObject eObject) {
-			contents.add(eObject);
-		} else if (value instanceof Object[] array) {
-			for (Object element : array) {
-				collect(element, contents);
-			}
-		} else if (value instanceof Iterable<?> iterable) {
-			for (Object element : iterable) {
-				collect(element, contents);
-			}
-		}
-	}
-
-	@Override
-	public Order getOrder() {
-		return sourceHandler.getOrder().add(0);
+		super(resource, sourceHandler);
+		// TODO EPackages
 	}
 
 }
